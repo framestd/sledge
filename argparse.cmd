463ff7410ff58c4a2bbd02a4122ea2ed2bac7b4f
CalebPitan <calebpitan@outlook.com>
2019-01-31 16:16:29 +0100

    command line arg parse for sledge
    Note we may remove this soon and handle it using python argparse, i.e our argparse would be done using python

---
+++ argparse.cmd
@@ -0,0 +1,103 @@
+@ECHO OFF
+set SPECURL=https://framestd.github.io/remarkup/spec/x/
+set RMH=https://framestd.github.io/sledge/
+set SPECMD=-spec
+set RM=remarkup
+set RHELP=--help
+set VER=--version
+set SLH=help
+set SLL=license
+set SLU=-u
+set SLC=contact
+set SLI=issue
+set RMV=Remarkup version X.0
+set SLV=Sledge v1.0
+set CONTACT=framestd@gmail.com
+set ISSUEURL=https://github.com/framestd/sledge/issues
+
+if %1 == %SPECMD% (
+    start %SPECURL%
+    goto END
+)
+
+for /F "tokens=1-4" %%G in ("%*") do (
+    if %%G == %RM% (
+        if %%H == %RHELP% (
+            set ARGS=%RMH% %%I
+            call :FURTHER %ARGS%
+            goto END
+        ) else if %%H == %VER% (
+            call :INFORM %%I
+            goto END
+        ) else (
+            goto RMHELPF
+        )
+    ) else if %%G == %VER% (
+        if "%%H" == "" (
+            echo %SLV%
+            goto END
+        ) else (
+            goto SLF
+        )
+    ) else if %%G == %SLH% (
+        if "%%H" == "" (
+            goto SLEDGEHELP
+        ) else (
+            goto SLF
+        )
+    ) else if %%G == %SLL% (
+        if "%%H" == "" (
+            more LICENSE
+            goto END
+        ) else if %%H == %SLU% (
+            more LICENSE
+        ) else (
+            goto SLF
+        )
+    ) else if %%G == %SLC% (
+        if "%%H" == "" (
+            echo %CONTACT%
+            goto END
+        ) else (
+            goto SLF
+        )
+    ) else if %%G == %SLI% (
+        if "%%H" == "" (
+            start %ISSUEURL%
+            goto END
+        ) else (
+            goto SLF
+        )
+    ) else (
+        goto END
+    )
+)
+
+:FURTHER
+if "%2" == "" (
+    start %1
+    goto END 
+) else (
+    goto RMHELPF
+)
+
+:INFORM
+if "%1" == "" (
+    echo %RMV%
+    goto END
+) else (
+    goto RMHELPF
+)
+
+:SLF
+echo type "sledge help" to get help or "sledge --version" to know the version of sledge
+goto END
+
+:RMHELPF
+echo type "sledge remarkup [--help/-h]" to get help or "sledge remarkup --version" to know the version you are using
+goto END
+
+:SLEDGEHELP
+type HELP.txt
+
+:END
