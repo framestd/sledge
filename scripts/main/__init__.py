f4b0ef2e613e54bab03df816c56ace21591dea11
CalebPitan <calebpitan@outlook.com>
2019-02-03 16:22:26 +0100

    now uses watchdog to watch file changes and build immediately

--- scripts/main/__init__.py
+++ scripts/main/__init__.py
@@ -23,6 +23,8 @@ _ptrns = [r"(?:\..+)$", r"([ \t]*)\$\{FRAME::BODY\}",
          r"\$\{FRAME::BODY\}", r"\$\{FRAME::LASTMOD\}", 
          r"\$\{FRAME::METAS::%s\}"]
 
+
+
 def render(src, mode=0):
     fr = compiler.Frame()
     fr.inform(basespace, workshop)
@@ -42,13 +44,13 @@ def get_all_files(basedir, ignore=(), _filter=()):
     for i in range(len(_filter)):
         for each in temp:
             if each.endswith(_filter[i]):
-                __build(basedir, each, render(os.path.join(basedir, each)))
+                _build(basedir, each, render(os.path.join(basedir, each)))
     for eachdir in dirsOnly:
         if os.path.isdir(os.path.join(basedir, eachdir)) and not eachdir in ignore:
             get_all_files(os.path.join(basedir, eachdir), ignore, filter)
     del allfiles, temp
 
-def __build(basedir, filename, response):
+def _build(basedir, filename, response):
     cLayoutFrame = render(response[0], 1)
     cMainFrame = response[1]
     dest = response[2]
@@ -59,10 +61,10 @@ def __build(basedir, filename, response):
     fileo = nfc = None
     try:
         fileo = open(genHTMLFile, 'w')
-        cMainFrame = __doTabs(cMainFrame, tab)
+        cMainFrame = _doTabs(cMainFrame, tab)
         nfc = re.sub(_ptrns[2], cMainFrame, cLayoutFrame) #layout body
         nfc = re.sub(_ptrns[3], specific["title"], nfc) #page title
-        for c in __metas(specific["meta"], nfc): #page meta tags
+        for c in _metas(specific["meta"], nfc): #page meta tags
             nfc = c
         fileo.write(nfc)
     except IOError as ex:
@@ -78,14 +80,14 @@ def __build(basedir, filename, response):
             console.error(status[0])
     return 0
 
-def __doTabs(context=nullstr, tab=nullstr):
+def _doTabs(context=nullstr, tab=nullstr):
     lctx = context.split(n)
     nctx = r''
     for each in lctx:
         nctx += "%s%s\n"%(tab,each)
     return nctx.rstrip()
 
-def __metas(x, c):
+def _metas(x, c):
     if type(x) is str:
         yield re.sub(_ptrns[6]%(r".+?"), x, c)
         return
@@ -97,10 +99,27 @@ def __metas(x, c):
         c = re.sub(_ptrns[6]%key, value, c)
         yield c
 
+
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+class Vigilante(PatternMatchingEventHandler):
+    patterns = list(_filter).append("*.yml")
+
+    def vigil(self, event):
+        _build(None, os.path.basename(event.src_path), render(event.src_path))
+    
+    def on_modified(self, event):
+        self.vigil(event)
+
+    def on_created(self, event):
+        self.vigil(event)
+
+
+
 def hammer(workspace=os.path.dirname(__file__)):
     global basespace
     if os.path.isfile(workspace):
-        __build(None, os.path.basename(workspace), render(workspace))
+        _build(None, os.path.basename(workspace), render(workspace))
         return
     basespace = workspace
     igpath, flpath = os.path.join(workspace,ig), os.path.join(workspace,fl)
@@ -109,3 +128,19 @@ def hammer(workspace=os.path.dirname(__file__)):
     ignore = tuple(ignore)
     _filter = tuple(_filter)
     get_all_files(workspace, ignore, _filter)
+
+    ob = Observer()
+    ob.schedule(Vigilante(), path=workspace)
+    ob.start()
+    import time
+    try:
+        while True:
+            time.sleep(1)
+            if os.name == 'nt':
+                os.system('cls')
+            else:
+                os.system('clear')
+    except KeyboardInterrupt:
+        ob.stop()
+    ob.join()
+
