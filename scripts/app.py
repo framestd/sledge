4aeb0f7060334609e68fb6c0a1ca6b262180b4dd
CalebPitan <calebpitan@outlook.com>
2019-02-04 11:25:05 +0100

    added watch flag for command line to watch directory

--- scripts/app.py
+++ scripts/app.py
@@ -5,8 +5,9 @@ import main
 watch = False
 workspace = os.path.abspath(sys.argv[1])
 try:
-    if sys.argv[2] == "-w" or sys.argv[2] == "--watch":
+    if sys.argv[1] == "-w" or sys.argv[1] == "--watch":
         watch = True
+        workspace = os.path.abspath(sys.argv[2])
 except IndexError:
     pass
 print(watch)
