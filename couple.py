daf4206ac3ba0b15626027aa4514d124e582f57e
CalebPitan <calebpitan@outlook.com>
2019-02-05 17:39:11 +0100

    distribution packaging script

---
+++ couple.py
@@ -0,0 +1,54 @@
+import shutil
+import yaml
+import os
+import sys
+
+banner = """Copyright {}. All rights reserved.
+Remarkup v1.0 python implementation.
+{} v{}.
+Project Manager: {}.
+The Remarkup specifications that govern this implementation can be found at:
+https://framestd.github.io/remarkup/spec/v1/
+Developers Indulgent Program (DIP)
+Use of this source code is licensed under the {} LICENSE
+which can be found in the LICENSE file."""
+package = open("package.yml")
+pkg = yaml.load(package)
+pkg = pkg['package']
+banner = banner.format(pkg["copyright"], pkg['name'], pkg['version'], pkg['authors'][1], pkg['license'])
+banner = '# '.join(banner.splitlines(True))
+dist = pkg['dist']
+
+if not os.path.exists(dist):
+    os.mkdir(dist)
+
+def get_all_files(path, *ignore):
+    allfiles = os.listdir(path)
+    dirsOnly = os.listdir(path)
+    temp = []
+    for eachfile in allfiles:
+        if os.path.isfile(os.path.join(path, eachfile)):
+            temp.append(eachfile)
+            dirsOnly.remove(eachfile)
+    for each in temp:
+        couple(path, each)
+    for eachdir in dirsOnly:
+        if os.path.isdir(os.path.join(path, eachdir)) and not eachdir in ignore:
+            get_all_files(os.path.join(path, eachdir), *ignore)
+
+def couple(p, f):
+    print p, f
+    src = os.path.join(p,f)
+    dest = os.path.join(dist, f)
+    fs = open(src)
+    fc = fs.read()
+    fc = '# ' + banner + '\n\n' + fc
+    fd = open(dest, 'w')
+    fd.write(fc)
+    fs.close()
+    fd.close()
+
+absp = os.path.dirname(os.path.abspath(sys.argv[0]))
+path = os.path.join(absp, 'scripts', 'main')
+get_all_files(path, '.vs', '.vscode')
+raw_input('hello')
