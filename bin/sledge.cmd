564a338a653061d4398347f6208c8e9727264b9b
CalebPitan <calebpitan@outlook.com>
2019-03-06 16:28:29 +0100

    Windows command script for sledge

--- bin/sledge.cmd
+++ bin/sledge.cmd
@@ -1,16 +1,12 @@
 @echo off
-:: <---------------------------------------------->
-:: |----------------------------------------------|
-:: | Sledge Command Line API (CLAPI) Entry        |
-:: | made for Windows                             |
-:: | Copyright 2019 Frame Studios, Caleb Adepitan |
-:: | Licensed under MIT                           |
-:: |----------------------------------------------|
-:: <---------------------------------------------->
+:: Sledge Command Line API (CLAPI) Entry
+:: Copyright (c) 2019 Caleb Adepitan
+:: Licensed under the MIT License
+
+title Frame Studios -- Sledge
 
 setlocal
-
-set SCRIPT=sledge_cli
+set SCRIPT=%~dp0sledge_cli.py
 set PYTHON_EXE=python.exe
 %PYTHON_EXE% --version >NUL 2>&1
 if "%ERRORLEVEL%" == "0" goto init else goto no_python
@@ -18,8 +14,8 @@ if "%ERRORLEVEL%" == "0" goto init else goto no_python
 echo [error]: python command not found
 echo     you either don't have python installed or you have not included it in your PATH
 echo     if you have python, you should consider running the following command
-echo     "set PATH=%%PATH%%;C:\<python_directory>"--set PATH only for this session
-echo     "setx PATH=%%PATH%%;C:\<python_directory>"--set PATH permanently
+echo     "set PATH=%%PATH%%;C:\<python_directory>"::set PATH only for this session
+echo     "setx PATH=%%PATH%%;C:\<python_directory>"::set PATH permanently
 echo     replace "<python_directory>" with the directory where you have python installed
 goto end
 :init
